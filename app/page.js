deb442e7fdb62c10a75024b15c1578d7f9bb2e72
Kishan Kumar <krishankumar9943@gmail.com>
2023-07-15 13:30:24 +0530

    Only Sever Actions

--- app/page.js
+++ app/page.js
@@ -1,32 +1,5 @@
 import Form from "@/components/Form";
-import User from "@/models/userModel";
 
 export default function Home() {
-  // const addData = async () => {
-  //   try {
-  //     const response = await fetch("/api/user", {
-  //       method: "POST",
-  //       body: JSON.stringify({
-  //         name: "Kishan",
-  //         email: "test@gmail.com",
-  //       }),
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     });
-
-  //     if (response.ok) {
-  //       console.log("Created");
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   addData();
-  // };
-
   return <Form />;
 }
