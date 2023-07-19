22b881530372f25a29fd2cbdccb15766b3a05a7b
Kishan Kumar <krishankumar9943@gmail.com>
2023-07-19 21:49:15 +0530

    POST method created

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,10 +1,52 @@
 "use client";
 
+import { useState } from "react";
+
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const addData = async () => {
+    try {
+      const response = await fetch("/api/user", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          email,
+        }),
+      });
+      if (response.ok) {
+        console.log("Created");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await addData();
+  };
   return (
-    <form className="h-screen w-full flex flex-col justify-center items-center gap-2">
-      <input type="text" name="name" className="border-2 rounded-lg " />
-      <input type="email" name="email" className="border-2 rounded-lg " />
+    <form
+      className="h-screen w-full flex flex-col justify-center items-center gap-2 "
+      onSubmit={onSubmit}
+    >
+      <input
+        type="text"
+        name="name"
+        className="border-2 rounded-lg px-2"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        name="email"
+        className="border-2 rounded-lg px-2 "
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <button type="submit">Submit</button>
     </form>
   );
